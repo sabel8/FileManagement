42aa5530c4e080f0e43d4a6252cd4ae7f2ed6eb0
ati <ati@venusz>
2018-04-25 23:01:15 +0200

    upgrade image renamer vertification

--- src/feladatok/RenameJPG.java
+++ src/feladatok/RenameJPG.java
@@ -1,18 +1,18 @@
 package feladatok;
 
-import java.awt.HeadlessException;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author ati
  */
-public class RenameJPG extends JFrame{
+public class RenameJPG extends JFrame {
 
     JFileChooser fc;
     static int n = 0;
@@ -20,18 +20,24 @@ public class RenameJPG extends JFrame{
     public RenameJPG() {
         fc = new JFileChooser();
         fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        fc.showOpenDialog(this);
+        if (fc.showOpenDialog(this) != JFileChooser.APPROVE_OPTION) {
+            JOptionPane.showMessageDialog(this, "Érvénytelen mappa!", "Hiba",
+                    JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
         File dir = fc.getSelectedFile();
         for (File img : dir.listFiles()) {
             if (img.getName().endsWith(".jpg")) {
                 try {
-                    img.renameTo(new File(img.getParentFile().getCanonicalPath() +"\\"+ ++n + ".jpg"));
+                    img.renameTo(new File(img.getParentFile().getCanonicalPath()
+                            + "\\" + ++n + ".jpg"));
                 } catch (IOException ex) {
-                    Logger.getLogger(RenameJPG.class.getName()).log(Level.SEVERE, null, ex);
+                    System.err.println(ex);
                 }
             }
         }
-        System.out.println("Változások: " + n);
+        String msg = "Sikeres átnevezés\nÁtnevezett fájlok: " + n;
+        JOptionPane.showMessageDialog(this, msg, "Siker", JOptionPane.INFORMATION_MESSAGE);
         System.exit(0);
     }
 
